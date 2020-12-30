00a77f2de5b8122db3833ed348f8075d364e892d
unknown <piyushrocks3108@gmail.com>
2020-12-30 13:39:20 +0530

    added connection and disconnection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 const path= require("path");
 const express= require('express');
+const http= require('http');
+
+const socketIO= require('socket.io');
+
 const publicPath= path.join(__dirname , "/../public");
 
 const port= process.env.PORT || 3000;
 
-var app= express();
+let app= express();
+let server= http.createServer(app);
+
+let io= socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on('connection', (socket)=>{
+    console.log("A new user has joined / connected ");
+
+    socket.on('disconnect',()=>{
+        console.log("User disconnected");
+    })
+});
+
+
+
+server.listen(port, ()=>{
     console.log("Listening to server");
 })
 
