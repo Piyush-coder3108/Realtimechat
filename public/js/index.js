abf8c3f68abf816c56eb4565f6d793bddf151678
unknown <piyushrocks3108@gmail.com>
2020-12-30 15:47:11 +0530

    Made proper connections

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,57 @@
 let socket= io();
-var name=prompt("Enter your name");
-   socket.on('connect',()=>{
-       console.log("connected");
+// var name=prompt("Enter your name");
+//    socket.on('connect',()=>{
+//        console.log("connected");
 
-       socket.emit('createMessage',{
-           from: name,
-           text : "Hello Buddies"
-       })
-   });
+//        socket.emit('createMessage',{
+//            from: name,
+//            text : "Hello Buddies"
+//        })
+//    });
 
-   socket.on('disconnect',()=>{
-       console.log("disconnected");
-   });
+//    socket.on('disconnect',()=>{
+//        console.log("disconnected");
+//    });
 
-   socket.on('newMessage',(message)=>{
-       console.log(`Server received this message ${message.from} and ${message.text} and ${message.createdat}`);
-   })
+//    socket.on('newMessage',(message)=>{
+//        console.log(`Server received this message ${message.from} and ${message.text} and ${message.createdat}`);
+//    })
+const form=document.getElementById("send-container");
+
+const messageinput=document.getElementById('messageinp');
+
+const messagecontainer= document.querySelector(".container");
+
+
+const name= prompt("Enter your name: ");
+console.log(name);
+
+const append=(message,position)=>{
+    const messageElement= document.createElement('div');
+    messageElement.innerHTML=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messagecontainer.append(messageElement);
+
+}
+
+form.addEventListener('submit',(e)=>{
+ e.preventDefault();
+ const message=messageinput.value;
+ append(`You: ${message}`,`right`);
+ socket.emit('send',message);
+ messageinput.value="";
+})
+socket.emit('new-user-joined',name);
+
+socket.on('user-joined', data=>{
+append(`${data} joined the chat`,'center')
+})
+
+socket.on('receive', data =>{
+    append(`${data.name}:  ${data.message}`,`left`)
+})
+
+socket.on('left', data =>{
+    append(`${data} has left the chat `,`center`)
+})
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const http= require('http');
 
 const socketIO= require('socket.io');
 
+const users={};
 const publicPath= path.join(__dirname , "/../public");
 
 const port= process.env.PORT || 3000;
@@ -16,23 +17,40 @@ let io= socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket)=>{
-    console.log("A new user has joined / connected ");
+    // console.log("A new user has joined / connected ");
     
     
 
 
-    socket.on('createMessage',(message)=>{
-        console.log(`Received message is from: ${message.from} and message is: ${message.text}`);
-        socket.broadcast.emit('newMessage',{
-            from: message.from,
-            text: "New user Joined the chat",
-            createdat: new Date().getTime()
-        })
+    // socket.on('createMessage',(message)=>{
+    //     console.log(`Received message is from: ${message.from} and message is: ${message.text}`);
+    //     socket.broadcast.emit('newMessage',{
+    //         from: message.from,
+    //         text: "New user Joined the chat",
+    //         createdat: new Date().getTime()
+    //     })
+    // });
+
+    // socket.on('disconnect',()=>{
+    //     console.log("User disconnected");
+    // })
+
+    socket.on('new-user-joined',name =>{
+        // console.log("User joined is :",name);
+        users[socket.id]=name;
+        socket.broadcast.emit('user-joined',name);
     });
 
-    socket.on('disconnect',()=>{
-        console.log("User disconnected");
-    })
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('receive',{message: message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect',message =>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+
 });
 
 
